463d3295be9dac9d57d282f58dd2593820c9b816
risema <richi2001.rsm@gmail.com>
2023-06-17 08:58:52 +0700

    push 1

---
+++ script_shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Menerima variabel dari aplikasi web
+nama=$1
+umur=$2
+alamat=$3
+
+# Menggunakan variabel dalam script shell
+echo "Nama: $nama"
+echo "Umur: $umur"
+echo "Alamat: $alamat"
