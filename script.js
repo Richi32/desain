e7540a613c7c037fad64b490e707323bca6acac6
risema <richi2001.rsm@gmail.com>
2023-06-17 09:29:31 +0700

    push  2

---
+++ script.js
@@ -0,0 +1,13 @@
+function processForm() {
+    var form = document.getElementById("myForm");
+    var formData = new FormData(form);
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "process_form.sh", true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            document.getElementById("result").innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send(formData);
+}
